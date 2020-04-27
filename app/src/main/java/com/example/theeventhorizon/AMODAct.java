395e537badf3f7efb30836cb866bbb9690f4c079
TanKumar <tk11@illinois.edu>
2020-04-27 15:52:58 -0500

    app not compiling, made new activity + button for AMOD, not sure if it's working since emulator won't run

---
+++ app/src/main/java/com/example/theeventhorizon/AMODAct.java
@@ -0,0 +1,14 @@
+package com.example.theeventhorizon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AMODAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amod);
+    }
+}
--- app/src/main/java/com/example/theeventhorizon/MainActivity.java
+++ app/src/main/java/com/example/theeventhorizon/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.theeventhorizon;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,23 +13,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toolbar;
+import android.widget.Button;
+//import android.widget.Toolbar;
 
 import com.example.theeventhorizon.ui.main.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button amodButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
+        amodButton = findViewById(R.id.amod_button);
+        amodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAMOD();
+
+            }
+        });
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        //SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager(),tabs.getTabCount());
+
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        //viewPager.addOnPageChangeListener(new TabLayout().TabLayoutOnPageChangeListener);
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Space Tab"));
+        tabs.addTab(tabs.newTab().setText("APOD Tab"));
+        tabs.addTab(tabs.newTab().setText("SpaceX Tab"));
+        tabs.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabs.setupWithViewPager(viewPager);
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,7 +77,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.tabs);
 
+    }
+
+    public void openAMOD() {
+        Intent amodIntent = new Intent(this, AMODAct.class);
+        startActivity(amodIntent);
     }
 }
